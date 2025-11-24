efe04344bedb783edf88936e782fe190efddc557
Harshitcoding <khandelwalharshit96@gmail.com>
2025-11-24 10:43:06 +0530

    added frontend url on the backend side

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,9 @@ const cookieParser = require('cookie-parser');
 const app = express()
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",  // tumhari Vite frontend URL
+  origin: ["http://localhost:5173",
+    "https://pixlemint.vercel.app"
+  ],
   credentials: true
 }));
 
