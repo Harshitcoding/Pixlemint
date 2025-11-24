2865cd583fd10247e882a93a20a8c489e5e37f21
Harshitcoding <khandelwalharshit96@gmail.com>
2025-11-24 11:16:35 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const app = require('./app');
-const PORT = 3000 
+const PORT = process.env.FRONTEND_URL || 3000 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
 })
